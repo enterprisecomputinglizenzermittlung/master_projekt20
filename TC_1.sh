c13ce1f981b7a8eaccb7aed588b66ae5d5a7e65f
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-08-26 18:14:23 +0200

    Update TC_1.sh

--- TC_1.sh
+++ TC_1.sh
@@ -1,6 +1,6 @@
 cd ~/scancode 
 ./extractcode ~/build/enterprisecomputinglizenzermittlung/master_projekt20/emacs-common-non-dfsg_26.1+1-1_all.deb
-./scancode -clpeui -n 2 ~/build/enterprisecomputinglizenzermittlung/master_projekt20/emacs-common-non-dfsg_26.1+1-1_all
+./scancode -clpeui -n 2 ~/build/enterprisecomputinglizenzermittlung/master_projekt20/emacs-common-non-dfsg_26.1+1-1_all.deb-extract
 cd ~/build/enterprisecomputinglizenzermittlung/master_projekt20/
 ls
 exit 0
