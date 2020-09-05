37b4b07ec8df225b1636d1db9dd1033dcad7bef1
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-05 16:47:18 +0200

    Update Tern.sh

--- Tern.sh
+++ Tern.sh
@@ -2,6 +2,7 @@ cd ~/tern
 sudo python3 -m venv ternenv
 cd ternenv
 source bin/activate
+sudo pip install wheel
 sudo cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py lib/python3.6/site-packages/wheel/setup.py
 python lib/python3.6/site-packages/wheel/setup.py bdist_wheel
 pip install tern
