284b534ef26d2d8e5f3378da1b0d1fec40ed99d9
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-05 16:13:06 +0200

    Update Tern.sh

--- Tern.sh
+++ Tern.sh
@@ -1,4 +1,6 @@
-
+cd ~/build/enterprisecomputinglizenzermittlung/master_projekt20/
+ls 
+cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py lib/python3.6/site-packages/wheel/
 cd ~/tern
 sudo python3 -m venv ternenv
 cd ternenv
