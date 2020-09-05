e8a11e05702130f6b7d88b1a39539f0ec7207d86
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-05 17:11:36 +0200

    Update Tern.sh

--- Tern.sh
+++ Tern.sh
@@ -2,9 +2,10 @@ cd ~/tern
 sudo python3 -m venv ternenv
 cd ternenv
 source bin/activate
-sudo pip install wheel
-sudo cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py lib/python3.6/site-packages/wheel/setup.py
-python lib/python3.6/site-packages/wheel/setup.py bdist_wheel
+#/home/travis/virtualenv/python3.6.10/lib/python3.6/site-packages/wheel/setup.py lib/python3.6/site-packages/wheel/setup.py
+sudo cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py /home/travis/virtualenv/python3.6.10/lib/python3.6/site-packages/wheel/setup.py
+#lib/python3.6/site-packages/wheel/setup.py bdist_wheel
+python /home/travis/virtualenv/python3.6.10/lib/python3.6/site-packages/wheel/setup.py bdist_wheel
 pip install tern
 tern report -o output.txt -i debian:buster
 ls
