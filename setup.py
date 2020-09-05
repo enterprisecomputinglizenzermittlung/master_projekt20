e50e99aaaff951c6c8e94f83c85b416b05aaf957
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-05 16:00:26 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+
+    name="user", # Replace with your username
+
+    version="1.0.0",
+
+    author="user",
+
+    author_email="<authorname@templatepackage.com>",
+
+    description="<Template Setup.py package>",
+
+    long_description_content_type="text/markdown",
+
+    url="<https://github.com/authorname/templatepackage>",
+
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Operating System :: OS Independent",
+
+    ],
+
+    python_requires='>=3.6',
+
+)
