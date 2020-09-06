10bcb5c2234e22694cb9a21f8d2dce26312e0802
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-06 12:58:27 +0200

    Update Tern_with_Scancode.sh

--- Tern_with_Scancode.sh
+++ Tern_with_Scancode.sh
@@ -1,1 +1,19 @@
+echo Step 1: Fulfilling prerequisites
+sudo apt-get install attr
+sudo apt-get install python3.6
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-wheel
+sudo apt-get install python3-pip
 
+echo Step 2: Create Python3 Venv 
+cd ~/tern
+sudo python3 -m venv ternenv
+
+echo Step 3: Activate Python3 Venv 
+cd ternenv
+sudo source bin/activate
+
+echo Step 4: Install Tern and Scancode Toolkit
+sudo python3 -m pip install tern scancode-toolkit
+
+exit $?
