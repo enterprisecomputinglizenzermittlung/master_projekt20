c255f1ee7f2ece146975f53a1a099af142eff082
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-06 12:08:28 +0200

    Update Tern.sh

--- Tern.sh
+++ Tern.sh
@@ -1,7 +1,18 @@
+echo Create Python3 Venv 
 cd ~/tern
 sudo python3 -m venv ternenv
+
+echo Copy setup.py into Tern Venv directory
+sudo cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py ~/tern/ternenv/lib/python3.6/site-packages/wheel/setup.py
+cd ~/tern/ternenv/lib/python3.6/site-packages/wheel
+ls
+cd ~/tern
+
+echo Activate Python3 Venv 
 cd ternenv
 source bin/activate
+
+echo Install Tern
 pip install tern
 
 exit 0
