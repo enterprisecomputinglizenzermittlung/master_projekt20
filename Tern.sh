85c445f49c82a0fdc7413e1fd48bda20b9719379
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-06 12:46:56 +0200

    Update Tern.sh

--- Tern.sh
+++ Tern.sh
@@ -1,31 +1,19 @@
-echo Looking for Python Binaries
-cd /usr/bin/python
-ls
-
-echo Fulfilling prerequisites
+echo Step 1: Fulfilling prerequisites
 sudo apt-get install attr
 sudo apt-get install python3.6
 sudo apt-get install python3-setuptools
 sudo apt-get install python3-wheel
 sudo apt-get install python3-pip
 
-echo Create Python3 Venv 
+echo Step 2: Create Python3 Venv 
 cd ~/tern
 sudo python3 -m venv ternenv
 
-echo Activate Python3 Venv 
+echo Step 3: Activate Python3 Venv 
 cd ternenv
 sudo source bin/activate
 
-echo Copy setup.py into Tern Venv directory
-sudo cp ~/build/enterprisecomputinglizenzermittlung/master_projekt20/setup.py ~/tern/ternenv/lib/python3.6/site-packages/wheel/setup.py
-cd ~/tern/ternenv/lib/python3.6/site-packages
-ls
-cd wheel
-ls
-cd ~/tern/ternenv
-
-echo Install Tern
+echo Step 4: Install Tern
 sudo python3 -m pip install tern
 
 exit $?
