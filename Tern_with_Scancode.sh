2481d6ab4161841fb573cfdff6355a7c372018dd
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-06 16:30:23 +0200

    Update Tern_with_Scancode.sh

--- Tern_with_Scancode.sh
+++ Tern_with_Scancode.sh
@@ -16,4 +16,7 @@ sudo source bin/activate
 echo Step 4: Install Tern and Scancode Toolkit
 sudo python3 -m pip install tern scancode-toolkit
 
+echo Step 5: Execute Testscan with Tern + Scancode
+tern report -x scancode -i golang:1.12-alpine
+
 exit $?
