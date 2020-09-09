bd917c657a5666742a00b04843137836859992c6
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-09 18:05:01 +0200

    Update Fossology.sh

--- Fossology.sh
+++ Fossology.sh
@@ -21,8 +21,18 @@ sudo /usr/local/etc/fossology/mods-enabled/scheduler/agent/fo_scheduler -t
 echo Step 6: Start Scheduler Daemon
 sudo /etc/init.d/fossology start
 
-echo Steop 7: -- MISSING REPORT COMMAND --
-./src/nomos/agent/nomos LICENSE
-./src/ninka/agent/ninka LICENSE
+echo Steop 7: Ninka and Nomos
+echo Nomos
+cd src/nomos
+sudo make install 
+sudo nomos ../../LICENSE
+sudo ./agent/nomos ../../LICENSE
+cd ../..
+
+echo Ninka
+cd src/ninka
+sudo make install
+sudo ninka ../../LICENSE
+sudo ./agent/ninka ../../LICENSE
 
 exit $?
