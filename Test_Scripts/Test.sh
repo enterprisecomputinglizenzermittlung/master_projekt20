719f7a8eb0513c49e2d2ec09a7d688e55e0ba152
Andreas Zahn <zahnan64088@th-nuernberg.de>
2020-09-13 17:29:05 +0200

    Cleaned deb package creation File

--- Test_Scripts/Test.sh
+++ Test_Scripts/Test.sh
@@ -1,10 +1,10 @@
-cd ..
+PACKAGE_BASE_DIR=~/hello-world
+
+echo Install prerequisites
 sudo apt-get install build-essential
 
-ls
-mkdir -p ~/hello-world
-
-echo cp Package_Creation_Sources/* ~/
+echo Copy Directories necessary for package creation to HOME
+cd ..
 cp -r Package_Creation_Sources/* ~/
 
 echo Copy GPL3.0 License File to Code
@@ -12,64 +12,40 @@ cd ~
 echo cp Licenses/GPL_3.0 Hello_World_Program/LICENSE
 cp Licenses/GPL_3.0 Hello_World_Program/LICENSE
 
-echo Create Copyright File for Debian Package
-mkdir -p ~/hello-world/usr/share/doc/package/
-cp Licenses/GPL_3.0 ~/hello-world/usr/share/doc/package/copyright
-cat ~/hello-world/usr/share/doc/package/copyright
+echo Create Package Base Dir
+mkdir -p $PACKAGE_BASE_DIR
 
-echo Append Apache License text to src
+echo Create Copyright File for Debian Package
+mkdir -p $PACKAGE_BASE_DIR/usr/share/doc/package/
+cp Licenses/GPL_3.0 $PACKAGE_BASE_DIR/usr/share/doc/package/copyright
+
+echo Append Apache License Text to src
 echo \/\* >> Hello_World_Program/src/main.cpp
 cat Licenses/Apache_2.0 >> Hello_World_Program/src/main.cpp
 echo \*\/ >> Hello_World_Program/src/main.cpp
 
 echo Copy Code to Package dir
-echo cp -r Hello_World_Program/ hello-world/
-cp -r Hello_World_Program/ hello-world/
+cp -r Hello_World_Program/ $PACKAGE_BASE_DIR
 
-echo Make Hello_World_Program
-echo cd ~/Hello_World_Program
+echo Compile Hello_World_Program
 cd ~/Hello_World_Program
 make
-ls
 
-echo create DEBIAN Dir
-cd ../hello-world
+echo Copy Control File for Creation
+cd $PACKAGE_BASE_DIR
 mkdir DEBIAN
-ls
+cp -p ~/Package_Creation_Files/deb_control -p $PACKAGE_BASE_DIR/DEBIAN
 
-echo create control file
-touch DEBIAN/control
-echo Package: hello-world > DEBIAN/control
-echo Version: 1.0 >> DEBIAN/control
-echo Section: custom >> DEBIAN/control
-echo Priority: optional >> DEBIAN/control
-echo Architecture: all >> DEBIAN/control
-echo Essential: no >> DEBIAN/control
-echo Installed-Size: 1024 >> DEBIAN/control
-echo Maintainer: FAU >> DEBIAN/control
-echo Description: Print Hello World on the console >> DEBIAN/control
-echo  >> DEBIAN/control
-cat DEBIAN/control
+echo Create usr/bin directory for executable program
+mkdir -p usr/bin
 
-mkdir usr
-cd usr
-mkdir bin
-cd ..
-ls
-cd ..
-
-echo copy program to bin
-echo cp Hello_World_Program/app hello-world/usr/bin
-cp Hello_World_Program/app hello-world/usr/bin
+echo Copy Program to mkdir -p $PACKAGE_BASE_DIR/usr/bin
+cd ~
+cp Hello_World_Program/app $PACKAGE_BASE_DIR/usr/bin
 
 echo Create Package
 dpkg-deb --build hello-world
 
 RET=$?
-ls
-
-echo Extract Package
-ar x hello-world.deb
-ls
 
 exit $RET
