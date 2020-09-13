1211e388422a733790e99bc77153eea11248f926
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-13 20:32:31 +0200

    Update Fossology_Toolkit_-_Ninka_and_Nomos_Install.sh

--- Install_Scripts/Fossology_Toolkit_-_Ninka_and_Nomos_Install.sh
+++ Install_Scripts/Fossology_Toolkit_-_Ninka_and_Nomos_Install.sh
@@ -17,19 +17,13 @@ sudo make install
 echo Step 4: Run postinstall script
 sudo /usr/local/lib/fossology/fo-postinstall
 
-echo Step 5: Check installed correctly
-#sudo /usr/local/etc/fossology/mods-enabled/scheduler/agent/fo_scheduler -t
-
-echo Step 6: Start Scheduler Daemon
-#sudo /etc/init.d/fossology start
-
-echo Step 7: Nomos installation
+echo Step 5: Nomos Installation
 cd src/nomos
 sudo make install 
 sudo mv agent/nomos ~/
 cd ../..
 
-echo Step 8: Ninka installation
+echo Step 6: Ninka installation
 cd install/scripts
 sudo ./install-ninka.sh
 cd /usr/local/bin
