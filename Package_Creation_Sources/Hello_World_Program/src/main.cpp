fa0640e5ec7058a33cb5d15eb87d2122b6e6e123
Andreas Zahn <zahnan64088@th-nuernberg.de>
2020-09-13 13:29:48 +0200

    Sources and License texts for various OSS Licenses were added

---
+++ Package_Creation_Sources/Hello_World_Program/src/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int main() {
+	int ret;
+	
+	ret = printf("Hello world");
+	
+	return ret;
+}
--- Test_Scripts/Test.sh
+++ Test_Scripts/Test.sh
@@ -1,1 +1,37 @@
+cd ..
+sudo apt-get install build-essentials
+
+mkdir -p ~/Hello_World
+cp Package_Creation_Sources/* ~/
+
+cd ~/Hello_World_Program
+make
+
+ls
+
+cd ../Hello_World
+mkdir DEBIAN
+ls
+touch DEBIAN/control
+echo Package: Hello_World > DEBIAN/control
+echo Version: 1.0 >> DEBIAN/control
+echo Section: custom >> DEBIAN/control
+echo Priority: optional >> DEBIAN/control
+echo Architecture: all >> DEBIAN/control
+echo Essential: no >> DEBIAN/control
+echo Installed-Size: 1024 >> DEBIAN/control
+echo Maintainer: FAU >> DEBIAN/control
+echo Description: Print Hello World on the console >> DEBIAN/control
+cat DEBIAN/control
+
+mkdir /usr/bin
+cd ..
+echo copy program to bin
+cp Hello_World_Program/app Hello_World/usr/bin
+
+cp Hello_World_Program/app Hello_World/usr/bin
+
+dpkg-deb --build Hello_World
+ls
+
 exit 0
