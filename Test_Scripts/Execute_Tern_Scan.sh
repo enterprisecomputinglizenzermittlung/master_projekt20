f4472c6dfe309a510aa24756e15bce9508c26470
enterprisecomputinglizenzermittlung <69750791+enterprisecomputinglizenzermittlung@users.noreply.github.com>
2020-09-13 18:31:50 +0200

    Update Execute_Tern_Scan.sh

--- Test_Scripts/Execute_Tern_Scan.sh
+++ Test_Scripts/Execute_Tern_Scan.sh
@@ -4,6 +4,6 @@ echo $0
 
 # No Container image has been created for this project yet. Only existing images from other projects could be scanned
 # Executing test scan for debian:buster
-sudo tern report -i debian:buster
+# sudo tern report -i debian:buster
 
 exit $?
